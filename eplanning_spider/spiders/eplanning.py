3777a8e0886207d53bc5ffad0d8c136d37e29928
Ahmed Bafadal <a.m.bafadal@gmail.com>
2018-07-18 22:08:34 +0100

    Developed capability to scrape form requests.

--- eplanning_spider/spiders/eplanning.py
+++ eplanning_spider/spiders/eplanning.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 from scrapy import Spider
-from scrapy.http import Request
+from scrapy.http import Request, FormRequest
 
 
 class EplanningSpider(Spider):
@@ -22,5 +22,11 @@ class EplanningSpider(Spider):
 
         yield Request(response.urljoin(app_url), callback=self.parse_form)
 
+    # dont_filter = True to prevent filtering the same url
     def parse_form(self, response):
+        yield FormRequest.from_response(response, formdata={'RdoTimeLimit': '42'},
+                                        dont_filter=True, formxpath='(//form)[2]',
+                                        callback=self.parse_pages)
+
+    def parse_pages(self, response):
         pass
